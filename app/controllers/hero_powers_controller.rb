694b391e8572d88920a7b3cd1ad31e7f2684e1b9
Sharon Awuor <sawuor395@gmail.com>
2023-08-22 09:13:51 +0300

    heropower

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,16 @@
-class HeroPowersController < ApplicationController
-end
+
+    class HeroPowersController < ApplicationController
+        def create
+          hero = Hero.find(params[:hero_id])
+          power = Power.find(params[:power_id])
+          hero_power = hero.hero_powers.build(power: power, strength: params[:strength])
+      
+          if hero_power.save
+            render json: hero.as_json(include: :powers)
+          else
+            render json: { errors: hero_power.errors.full_messages }, status: :unprocessable_entity
+          end
+        end
+      end
+      
+
